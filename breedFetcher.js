c995495f4473477517859d241bdf2e0ff462db9d
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-12 23:11:04 +0000

    Command line app that allows to fetch raw data

---
+++ breedFetcher.js
@@ -0,0 +1,18 @@
+const request = require("request");
+
+const breedFetcher = () => {
+  const args = process.argv;
+  //Value we want is at index 2 (print args to check)
+  const breed = args[2];
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log("Cat not found!");
+    }
+    if (error) {
+      console.log(error);
+    }
+    console.log(data[0].description);
+  });
+};
+breedFetcher();
